399ef24b424506f6426ef0ed27166f0c5b291762
josue <josue.patino@epn.edu.ec>
2025-11-10 12:00:01 -0500

    ACTUALIZACION

--- src/mainVehiculo.java
+++ src/mainVehiculo.java
@@ -3,9 +3,23 @@ import java.util.Scanner;
 public class mainVehiculo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Moto m1 = new Moto("A001","TOYOTA",2025,"KEVIN");
-        Auto a1 = new Auto("AB02","CHEVROLET",2012,"MICHAEL");
-        Camion c1 = new Camion("ACC0","MAZDA",2002,"KEVIN");
+
+        System.out.println("Ingrese la placa del vehiculo:");
+        String placa = sc.nextLine();
+
+        System.out.println("Ingrese la marca del vehiculo:");
+        String marca = sc.nextLine();
+
+        System.out.println("Ingrese la anio del vehiculo:");
+        int anio = sc.nextInt();
+
+        System.out.println("Ingrese la propietario del vehiculo:");
+        String propietario = sc.nextLine();
+
+
+        Moto m1 = new Moto(placa, marca, anio, propietario);
+        Auto a1 = new Auto(placa, marca, anio, propietario);
+        Camion c1 = new Camion(placa, marca, anio, propietario);
 
         System.out.println("Calculo de matricula en auto\n");
         System.out.println(a1.calcularValorMatricula());
